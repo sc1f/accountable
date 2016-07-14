0a39c2e87c173520bc3b9cbfe8de06232e996565
sc1f <tanjunyuan@hotmail.co.uk>
2016-07-14 09:56:02 -0400

    more localization stuff that is also broken

--- routes/scrape.js
+++ routes/scrape.js
@@ -33,19 +33,74 @@ router.get('/generate-link', function(req, res) {
         }
     });
 
+    fs.writeFile('output.txt', function(err){
+        if (err) {
+            throw err;
+        }
+        console.log('File successfully written! - Check your project directory for the output.txt file');
+
+    })
+
     res.json({
         status: 'success!',
         action: 'generate links',
-        output: container
+        output: container // FIXME very very empty
     });
 
 });
 
-// TODO: localize the PDF assets for parsing
+// TODO: test localization
+
 router.get('/localize-pdf', function(req, res) {
-    res.json({
-        response: 'WIP'
-    })
+    var content, statusCheck = false; // base variables in global scope
+    // open the output file for parsing; we use a file from the filesystem because the variable
+    fs.readFile('output.txt', function read(data, err) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+        processContent(); // run processor
+    });
+
+    function processContent() {
+        if (content.length){
+            // if array has content, begin parse | throw error
+            for(var i = 0; i <= content.length; i++) {
+                // more dynamic vars
+                var url = content[i].link;
+                var filename = content[i].name;
+                var file = './assets/' + filename;
+
+                function downloadFile(url, path, callback) {
+                    request({uri: url}) // make the request
+                        .pipe(fs.createWriteStream(path)) // open a file write stream
+                        .on('close', function () { // on stream close, run callback
+                            callback(); // run through status
+                        });
+                }
+
+                downloadFile(url, file, function() {
+                   console.log(file + " has been downloaded from " + url); // another way to check file write status, FIXME: mutable variable accessible from closure
+                });
+            }
+        } else {
+            res.json({
+                response: 'Nothing to download!'
+            });
+        }
+        statusCheck = true; // return a statusCheck var for us to send a response to the user
+    }
+
+    if (statusCheck == true) {
+        res.json({
+            response: 'Files have been downloaded.'
+        });
+    } else {
+        res.json({
+            response: 'File downloader has failed.'
+        });
+    }
+
 });
 
 // TODO: parse the PDF files (externalize logic?)
